51600459d2d1906c16f223af3acb7d6a02c79f75
Bob Weichler <bobmasaki@hotmail.com>
2019-01-16 20:24:31 +0100

    Send Chat messages

--- src/components/chat/Sendchat.vue
+++ src/components/chat/Sendchat.vue
@@ -14,23 +14,45 @@
 			<div class="content">
 				<div class="field">
 					<div class="control">
-						<textarea class="textarea" placeholder="Send a message..."></textarea>
+						<textarea class="textarea" placeholder="Send a message..." v-model="chatMessage"></textarea>
 					</div>
 				</div>
 			</div>
 		</div>
 		<footer class="card-footer">
 			<a href="#" class="card-footer-item">Cancel</a>
-			<a href="#" class="card-footer-item">Send</a>
+			<a href="#" class="card-footer-item" @click.prevent="handleSubmit">Send</a>
 		</footer>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios'
+
 	export default{
 		data(){
 			return{
-				boxTitle: 'Join the conversation!'
+				boxTitle: 'Join the conversation!',
+				chatMessage: ''
+			}
+		},
+		methods: {
+			handleSubmit(){
+				const today = new Date();
+				let dd = today.getDate();
+				let mm = today.getMonth() + 1;
+				const yyyy = today.getFullYear();
+
+				dd = (dd < 10) ? '0' + dd : dd;
+				mm = (mm < 10) ? '0' + mm : mm;
+
+				const formData = {
+					chat: this.chatMessage,
+					date: `${dd}/${mm}/${yyyy}`
+				}
+				axios.post('https://vuechat-952ec.firebaseio.com/chats.json', formData)
+					.then(res => console.log(res))
+					.catch(err => console.error(err));
 			}
 		}
 	}
