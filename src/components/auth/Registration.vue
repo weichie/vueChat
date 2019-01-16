9d7ed2e135da6689e774f40f63a3cb860a738e62
Bob Weichler <bobmasaki@hotmail.com>
2019-01-16 20:05:36 +0100

    User registration to firebase (unsafe)

--- src/components/auth/Registration.vue
+++ src/components/auth/Registration.vue
@@ -1,11 +1,62 @@
 <template>
 	<div class="registration-component">
-		<h1>Registration</h1>
+		<h1 class="title is-1">Registration</h1>
+		<form action="" @submit.prevent="onSubmit">
+			<div class="field">
+				<label class="label">Username</label>
+				<div class="control">
+					<input class="input" type="text" placeholder="Your name" v-model="username">
+				</div>
+			</div>
+			<div class="field">
+				<label class="label">Email</label>
+				<div class="control">
+					<input class="input" type="email" placeholder="john.doe@gmail.com" v-model="email" />
+				</div>
+			</div>
+			<div class="field">
+				<label class="label">Password</label>
+				<div class="control">
+					<input class="input" type="password" placeholder="***" v-model="password">
+				</div>
+			</div>
+			<div class="field is-grouped">
+				<div class="control">
+					<button class="button is-link">Register</button>
+				</div>
+				<div class="control">
+					<button class="button is-text">Cancel</button>
+				</div>
+			</div>
+		</form>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios';
+	
 	export default{
-
+		data(){
+			return{
+				email: '',
+				username: '',
+				password: ''
+			}
+		},
+		methods: {
+			onSubmit(){
+				const formData = {
+					email: this.email,
+					username: this.username,
+					password: this.password
+				}
+				console.log(formData);
+				axios.post('https://vuechat-952ec.firebaseio.com/users.json', formData)
+					.then(res => {
+						console.log(res);
+					})
+					.catch(err => console.error(err));
+			}
+		}
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import firebase from 'firebase'
 
 import VueRouter from 'vue-router';
 import { routes } from './routes';
@@ -8,8 +7,6 @@ import store from './store/store'
 
 import 'bulma/css/bulma.css'
 import './assets/style.css'
-
-import config from './firebaseconfig'
 
 Vue.use(VueRouter);
 
